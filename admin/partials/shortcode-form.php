9bfe6f4501151cb239dffc217845744ae0fdfc04
magiclogix <magiclogix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 20:56:57 +0000

    Updated Screenshots
    git-svn-id: https://plugins.svn.wordpress.org/msync/trunk@1159144 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/partials/shortcode-form.php
+++ admin/partials/shortcode-form.php
@@ -1,5 +1,5 @@
 <div id="msync_popup_container" style="display:none;">
-    <h3>Choose your Form</h3>
+    <h3>Select the appropriate form you wish to use. </h3>
     <br>
     <select name="marketo_form_id">
       <?php if(is_array($marketo_forms)):?>
@@ -8,6 +8,6 @@
         <?php endforeach;?>
       <?php endif;?>
     </select>
-    <p class="description">Description.</p>
+    <p class="description">Simply select the form template you think will fit best to use it within this post. If you need to edit a form or create a new form type, go to the MSync tab and either edit an existing one or create a new one.</p>
     <p><input type="button" class="button-primary" id="msync-add-shortcode-button" value="Insert Form"></p>
 </div>
